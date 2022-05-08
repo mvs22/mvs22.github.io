9ecaaa6672724bfbbfe21a9afb5641b6d3ae652d
gmsj <gmsj@cin.ufpe.br>
2022-05-08 19:08:41 -0300

    added style with bootstrap

--- final.js
+++ final.js
@@ -9,96 +9,95 @@ function getOfStorage(key) {
   return dados;
 }
 
-const sOriginal = ( sketch ) => {
+const sOriginal = (sketch) => {
 
   let indexWord = 0;
 
   let words;
 
-  sketch.preload = () =>{
+  sketch.preload = () => {
     sketch2 = sketch;
     let l;
 
-    var queryString = getOfStorage("letters");//location.search.substring(1);
+    var queryString = getOfStorage("letters"); //location.search.substring(1);
     words = queryString.split("|");
     xs = getOfStorage("xs");
     ys = getOfStorage("ys");
     count = 0
-    for(let i = 0; i < words.length;i++){
-      for(let j = 0; j < words[i].length;j++){
-        console.log('alfabeto/'+words[i][j].toUpperCase()+'.png')
-        l = new letter(sketch.loadImage('alfabeto/'+words[i][j].toUpperCase()+'.png'),xs[count],ys[count],50,50);
-        count+=1;
+    for (let i = 0; i < words.length; i++) {
+      for (let j = 0; j < words[i].length; j++) {
+        console.log('alfabeto/' + words[i][j].toUpperCase() + '.png')
+        l = new letter(sketch.loadImage('alfabeto/' + words[i][j].toUpperCase() + '.png'), xs[count], ys[count], 50, 50);
+        count += 1;
         imgs.push(l)
       }
     }
-    for(let i = 0; i < imgs.length;i++){
-      imgs[i].toggleOnOff(true,false);
+    for (let i = 0; i < imgs.length; i++) {
+      imgs[i].toggleOnOff(true, false);
     }
     let offset = 0;
-    for(let i_words = 0; i_words < words.length;i_words++){
+    for (let i_words = 0; i_words < words.length; i_words++) {
       let aux = []
       let minx = 999999;
       let miny = 999999;
       let maxw = 0;
       let maxh = 0;
-      for(let i = offset;i < offset+words[i_words].length;i++){
+      for (let i = offset; i < offset + words[i_words].length; i++) {
         aux.push(imgs[i]);
-        if(imgs[i].x < minx){
+        if (imgs[i].x < minx) {
           minx = imgs[i].x;
         }
-        if(imgs[i].y < miny){
+        if (imgs[i].y < miny) {
           miny = imgs[i].y;
         }
-        if(imgs[i].x + imgs[i].w > maxw){
+        if (imgs[i].x + imgs[i].w > maxw) {
           maxw = imgs[i].w + imgs[i].x;
         }
-        if(imgs[i].y + imgs[i].h > maxh){
+        if (imgs[i].y + imgs[i].h > maxh) {
           maxh = imgs[i].h + imgs[i].y;
         }
-      } 
-      let g = new glyph(aux,minx,miny,maxw,maxh);
+      }
+      let g = new glyph(aux, minx, miny, maxw, maxh);
       glyphs.push(g);
-      offset+=words[i_words].length;
-      indexWord+=1;
+      offset += words[i_words].length;
+      indexWord += 1;
     }
 
-    misturar()
-    
   }
 
   sketch.setup = () => {
     cOriginal = sketch.createCanvas(480, 360);
+    misturar()
   };
 
   sketch.draw = () => {
     sketch.background(0);
     //sketch.fill(255);
-    
-    for(let i = 0; i < imgs.length; i++){
+
+    for (let i = 0; i < imgs.length; i++) {
       imgs[i].show2(sketch)
     }
-    for(let i = 0; i < glyphs.length;i++){
+    for (let i = 0; i < glyphs.length; i++) {
       glyphs[i].update2(sketch);
       glyphs[i].over2(sketch);
     }
-    
+
   };
   sketch.mousePressed = () => {
-    console.log("SKETCH:" , sketch);
-    for(let i = 0; i < glyphs.length;i++){
+    console.log("SKETCH:", sketch);
+    for (let i = 0; i < glyphs.length; i++) {
       glyphs[i].pressed2(sketch);
     }
   }
-  
+
   sketch.mouseReleased = () => {
-    for(let i = 0; i < glyphs.length;i++){
+    for (let i = 0; i < glyphs.length; i++) {
       glyphs[i].released();
     }
   }
 
   sketch.over = () => {
-    for(let i = 0; i < glyphs.length;i++){
+    for (let i = 0; i < glyphs.length; i++) {
       glyphs[i].update2(sketch);
       glyphs[i].over2(sketch);
     }
@@ -106,7 +105,7 @@ const sOriginal = ( sketch ) => {
 
 };
 
-const sCaleidoscopio = ( sketch ) => {
+const sCaleidoscopio = (sketch) => {
 
   let indexWord = 0;
   let words;
@@ -118,47 +117,47 @@ const sCaleidoscopio = ( sketch ) => {
 
   sketch.draw = () => {
     sketch.background(0);
-    sketch.translate(sketch.width/2,sketch.height/2)
+    sketch.translate(sketch.width / 2, sketch.height / 2)
     let angle = 14
-    
-    for(let i = 0; i < angle;i++){
+
+    for (let i = 0; i < angle; i++) {
       sketch.rotate(angle)
       sketch.push()
-      sketch.scale(1,-1);
-      for(let i = 0; i < imgs.length; i++){
+      sketch.scale(1, -1);
+      for (let i = 0; i < imgs.length; i++) {
         //sketch.tint(imgs[i].x,imgs[i].y,100,1)
         //imgs[i].update()
         imgs[i].show2(sketch)
       }
-      for(let i = 0; i < glyphs.length;i++){
+      for (let i = 0; i < glyphs.length; i++) {
         //sketch.tint(glyphs[i].x,imgs[i].y,100,1)
         glyphs[i].update2(sketch);
         glyphs[i].over2(sketch);
       }
       sketch.pop()
     }
-    
+
   };
-  
+
 
 };
 
-function exportCaleidoscope(){
+function exportCaleidoscope() {
   sketch2.saveCanvas(cCaleidoscopio, 'myCanvas', 'jpg');
 }
 
-function exportOriginal(){
+function exportOriginal() {
   sketch2.saveCanvas(cOriginal, 'myCanvas', 'jpg');
 }
 
-function misturar(){
-  if(glyphs.length > 0 ){
-    for(let i = 0; i < glyphs.length;i++){
-      
-      glyphs[i].updateXY(Math.floor(Math.random()*(sketch2.width-glyphs[i].w)), Math.floor(Math.random()*(sketch2.height-glyphs[i].h)))
+function misturar() {
+  if (glyphs.length > 0) {
+    for (let i = 0; i < glyphs.length; i++) {
+
+      glyphs[i].updateXY(Math.floor(Math.random() * (sketch2.width - glyphs[i].w)), Math.floor(Math.random() * (sketch2.height - glyphs[i].h)))
     }
-  } 
+  }
 }
 
-let original = new p5(sOriginal,'original');
-let caleidoscopio = new p5(sCaleidoscopio,'caleidoscopio');
+let original = new p5(sOriginal, 'original');
+let caleidoscopio = new p5(sCaleidoscopio, 'caleidoscopio');
--- segunda_tela.js
+++ segunda_tela.js
@@ -1,4 +1,4 @@
-let c,c2;
+let c, c2;
 let imgs = [];
 let img_example;
 let glyphs = [];
@@ -10,77 +10,77 @@ let textWord;
 let name_image = 'exemplo_usuario/exemplo1.png';
 
 //Pega as palavras que foram inseridas pelo usuario (Ex: mikael|vidal|da|silva)
-function preload(){
+function preload() {
   console.log("PRELOADDDDDDDDDDDDDDDDDDDDDD");
   let l;
   var queryString = location.search.substring(1);
   words = queryString.split("|");
-  for(let i = 0; i < words.length;i++){
-    for(let j = 0; j < words[i].length;j++){
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words[i].length; j++) {
       //console.log('alfabeto/'+words[i][j].toUpperCase()+'.png')
-      l = new letter(loadImage('alfabeto/'+words[i][j].toUpperCase()+'.png'),40*j,50,50,50);
+      l = new letter(loadImage('alfabeto/' + words[i][j].toUpperCase() + '.png'), 40 * j, 50, 50, 50);
       imgs.push(l)
     }
-    
+
   }
 
-  
+
 }
 
 //Função responsavel por criar a tela preta (canvas, onde o DSCRIPT | PALAVRA | IMAGEM INSPIRAÇÃO) vão aparecer
 function setup() {
   console.log("SETUPPPPPPPPPPPPPPPPPPPPPPPPP");
-  img_example=loadImage(name_image);
+  img_example = loadImage(name_image);
   //colorMode(HSB,150,100,10,1)
   c = createCanvas(720, 640);
-  
+
   let offset = 0;
-  for(let i = 0; i < indexWord;i++){
-    offset+=words[i].length
+  for (let i = 0; i < indexWord; i++) {
+    offset += words[i].length
   }
-  for(let i = 0; i < offset; i++){
-    imgs[i].toggleOnOff(false,false);
+  for (let i = 0; i < offset; i++) {
+    imgs[i].toggleOnOff(false, false);
   }
-  for(let i = offset; i < offset+words[indexWord].length; i++){
-    imgs[i].toggleOnOff(true,true);
+  for (let i = offset; i < offset + words[indexWord].length; i++) {
+    imgs[i].toggleOnOff(true, true);
   }
-  indexWord+=1;
-  
+  indexWord += 1;
+
 }
 
 //Função para misturar as letras DSCRIPT na pagina
-function misturar(){
-  if(glyphs.length > 0 ){
-    for(let i = 0; i < glyphs.length;i++){
+function misturar() {
+  if (glyphs.length > 0) {
+    for (let i = 0; i < glyphs.length; i++) {
       //alert("glyph?")
-      glyphs[i].updateXY(Math.floor(Math.random()*(width-glyphs[i].w)), Math.floor(Math.random()*(height-glyphs[i].h)))
+      glyphs[i].updateXY(Math.floor(Math.random() * (width - glyphs[i].w)), Math.floor(Math.random() * (height - glyphs[i].h)))
     }
-  }else{
+  } else {
     let offset = 0;
-    for(let i = 0; i < indexWord-1;i++){
-      offset+=words[i].length
+    for (let i = 0; i < indexWord - 1; i++) {
+      offset += words[i].length
     }
-    for(let i = 0; i < offset+words[indexWord-1].length; i++){
-      imgs[i].x = Math.floor(Math.random()*(width-imgs[i].w))
-      imgs[i].y = Math.floor(Math.random()*(height-imgs[i].h))
+    for (let i = 0; i < offset + words[indexWord - 1].length; i++) {
+      imgs[i].x = Math.floor(Math.random() * (width - imgs[i].w))
+      imgs[i].y = Math.floor(Math.random() * (height - imgs[i].h))
     }
   }
-  
-  
+
+
 }
 
 //Função auxiliar
 function getCircularReplacer() {
   const seen = new WeakSet();
-    return (key, value) => {
-      if (typeof value === 'object' && value !== null) {
-        if (seen.has(value)) {
-          return;
-        }
-        seen.add(value);
+  return (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return;
       }
-      return value;
-    };
+      seen.add(value);
+    }
+    return value;
+  };
 }
 
 //Função para salvar estrutura feita pelo usuario com as letras em DSCRIPT
@@ -95,192 +95,191 @@ function draw() {
   background(0);
 
   textSize(30);
-  fill(255,255,255);
-  text(words[indexWord-1],320,600);
-  console.log("----------------------- ",words[indexWord-1]);
-  
-  if(kaledoscope == true){
-    translate(width/2,height/2)
+  fill(255, 255, 255);
+  text(words[indexWord - 1], 320, 600);
+  // console.log("----------------------- ",words[indexWord-1]);
+
+  if (kaledoscope == true) {
+    translate(width / 2, height / 2)
     let angle = 14
-    
-    for(let i = 0; i<angle;i++){
+
+    for (let i = 0; i < angle; i++) {
       rotate(angle)
       push()
-      scale(1,-1);
-      for(let i = 0; i < imgs.length; i++){
-        tint(imgs[i].x,imgs[i].y,100,1)
+      scale(1, -1);
+      for (let i = 0; i < imgs.length; i++) {
+        tint(imgs[i].x, imgs[i].y, 100, 1)
         imgs[i].update()
         imgs[i].show()
       }
-      for(let i = 0; i < glyphs.length;i++){
-        tint(glyphs[i].x,imgs[i].y,100,1)
+      for (let i = 0; i < glyphs.length; i++) {
+        tint(glyphs[i].x, imgs[i].y, 100, 1)
         //alert("glyph?")
         glyphs[i].update();
         glyphs[i].over();
       }
       pop()
     }
-  }else{
+  } else {
     //console.log("DRAWWWWWWWWWWWWWWWWWWWWWWWW2 ");
     //rect(30, 80, 255, 255);
-    for(let i = 0; i < imgs.length; i++){
+    for (let i = 0; i < imgs.length; i++) {
       imgs[i].update()
       imgs[i].show()
     }
-    for(let i = 0; i < glyphs.length;i++){
+    for (let i = 0; i < glyphs.length; i++) {
       //alert("glyph?")
       glyphs[i].update();
       glyphs[i].over();
     }
   }
 
-  image(img_example,0,500);
+  image(img_example, 0, 500);
 }
 
 //Função auxiliar
-function mousePressed(){
-  for(let i= 0;i < imgs.length; i++){
+function mousePressed() {
+  for (let i = 0; i < imgs.length; i++) {
     imgs[i].pressed();
   }
-  for(let i = 0; i < glyphs.length;i++){
+  for (let i = 0; i < glyphs.length; i++) {
     glyphs[i].pressed();
   }
 }
 //Função auxiliar
-function mouseReleased(){
-  for(let i = 0;i < imgs.length; i++){
+function mouseReleased() {
+  for (let i = 0; i < imgs.length; i++) {
     imgs[i].released()
   }
-  for(let i = 0; i < glyphs.length;i++){
+  for (let i = 0; i < glyphs.length; i++) {
     glyphs[i].released();
   }
 }
 //Função auxiliar
-function rectanglesIntersect(minAx,  minAy,  maxAx,  maxAy, minBx,  minBy,  maxBx,  maxBy ) {
-   aLeftOfB = maxAx < minBx;
-   aRightOfB = minAx > maxBx;
-   aAboveB = minAy > maxBy;
-   aBelowB = maxAy < minBy;
+function rectanglesIntersect(minAx, minAy, maxAx, maxAy, minBx, minBy, maxBx, maxBy) {
+  aLeftOfB = maxAx < minBx;
+  aRightOfB = minAx > maxBx;
+  aAboveB = minAy > maxBy;
+  aBelowB = maxAy < minBy;
 
-  return !( aLeftOfB || aRightOfB || aAboveB || aBelowB );
+  return !(aLeftOfB || aRightOfB || aAboveB || aBelowB);
 }
 //Função para verificar se a estrutura de DSCRIPT esta perto suficiente uma das outras
-function verificaSuperposicao(){
+function verificaSuperposicao() {
   var valido = true;
   let offset = 0;
-  for(let i = 0; i < indexWord-1;i++){
-    offset+=words[i].length
+  for (let i = 0; i < indexWord - 1; i++) {
+    offset += words[i].length
   }
-  for(let i = offset; i < offset+words[indexWord-1].length; i++){
+  for (let i = offset; i < offset + words[indexWord - 1].length; i++) {
     var validaLetra = false;
-    for(let j = offset; j < offset+words[indexWord-1].length; j++){
-      if(i == j){
+    for (let j = offset; j < offset + words[indexWord - 1].length; j++) {
+      if (i == j) {
         continue;
       }
-      if(rectanglesIntersect(imgs[i].x,imgs[i].y,imgs[i].x+imgs[i].w,imgs[i].y+imgs[i].h,imgs[j].x,imgs[j].y,imgs[j].x+imgs[j].w,imgs[j].y+imgs[j].h)){
+      if (rectanglesIntersect(imgs[i].x, imgs[i].y, imgs[i].x + imgs[i].w, imgs[i].y + imgs[i].h, imgs[j].x, imgs[j].y, imgs[j].x + imgs[j].w, imgs[j].y + imgs[j].h)) {
         validaLetra = true;
       }
     }
-    if(!validaLetra){
+    if (!validaLetra) {
       valido = false;
     }
   }
   return valido;
 }
 //Função auxiliar
-function toggleKaledoscope(){
-  if(kaledoscope){
-    kaledoscope=false;
-  }else{
-    kaledoscope=true;
+function toggleKaledoscope() {
+  if (kaledoscope) {
+    kaledoscope = false;
+  } else {
+    kaledoscope = true;
   }
 }
 
 //Função para salvar a estrutura atual de DSCRIPT e ir para a proxima palavra
-function updateOrSave(){
+function updateOrSave() {
   console.log("UPDATEEEEEE ORRRR SAVEEEEEE");
   //if(indexWord > words.length){
   //  saveCanvas(c, 'myCanvas', 'jpg');
   //}
-  if(indexWord == words.length){
-    
-    for(let i = 0; i < imgs.length;i++){
-      imgs[i].toggleOnOff(true,false);
+  if (indexWord == words.length) {
+
+    for (let i = 0; i < imgs.length; i++) {
+      imgs[i].toggleOnOff(true, false);
     }
     let offset = 0;
-    for(let i_words = 0; i_words < words.length;i_words++){
+    for (let i_words = 0; i_words < words.length; i_words++) {
       let aux = []
       let minx = 999999;
       let miny = 999999;
       let maxw = 0;
       let maxh = 0;
-      for(let i = offset;i < offset+words[i_words].length;i++){
+      for (let i = offset; i < offset + words[i_words].length; i++) {
         aux.push(imgs[i]);
-        if(imgs[i].x < minx){
+        if (imgs[i].x < minx) {
           minx = imgs[i].x;
         }
-        if(imgs[i].y < miny){
+        if (imgs[i].y < miny) {
           miny = imgs[i].y;
         }
-        if(imgs[i].x + imgs[i].w > maxw){
+        if (imgs[i].x + imgs[i].w > maxw) {
           maxw = imgs[i].w + imgs[i].x;
         }
-        if(imgs[i].y + imgs[i].h > maxh){
+        if (imgs[i].y + imgs[i].h > maxh) {
           maxh = imgs[i].h + imgs[i].y;
         }
-      } 
-      let g = new glyph(aux,minx,miny,maxw,maxh);
+      }
+      let g = new glyph(aux, minx, miny, maxw, maxh);
       glyphs.push(g);
-      offset+=words[i_words].length;
-      indexWord+=1;
+      offset += words[i_words].length;
+      indexWord += 1;
     }
-    
+
     //Salva a estrutura
-    saveInStorage("letters",location.search.substring(1));
+    saveInStorage("letters", location.search.substring(1));
     xs = []
     ys = []
-    for(let i = 0; i < imgs.length;i++){
+    for (let i = 0; i < imgs.length; i++) {
       xs.push(imgs[i].x)
       ys.push(imgs[i].y)
     }
-    saveInStorage("ys",ys);
-    saveInStorage("xs",xs);
-    
+    saveInStorage("ys", ys);
+    saveInStorage("xs", xs);
+
     window.location.replace("http://mvs22.github.io/final.html");
-    // window.location.replace("localhost:8000/final.html");
-  }else{
+  } else {
     //Se estiver tudo ok, ele remove a cena atual e inicia outra com aproxima palavra
-    if(verificaSuperposicao()){
+    if (verificaSuperposicao()) {
       //var data = canvas.toDataURL('image/png').replace(/data:image\/png;base64,/, '');
-    
+
       // make names  eg "img_1.png", "img_2.png"......etc"
       //var iname = 'img_' + index + '.png'; 
-      
+
       c.remove();
       //post to php
       //$.post('save.php',{data: data, iname });
       // update counter
       index++;
 
-      
+
       //restart sketch
       setup();
-      
-      
-    }else{
+
+
+    } else {
       alert('opa, as letras precisam se sobrepor!')
     }
-    
+
   }
-  
+
 }
 
 //Função utilizada para gerar um numero aleatorio para auxiliar no LOAD de uma imagem de INSPIRAÇÃO (pasta exemplo_inspiração)
 //Para podermos mostrar ao usuario, e ele se inspirar em criar alguma estrutura utilizando o DSCRIPT
-function example(){
-    let r = int(random(4));
-    console.log('RAMDON:',r);
-    img_example = loadImage('exemplo_usuario/exemplo' + str(r) + '.png');
-    //name_image = 'exemplo_usuario/exemplo2.png'
-    console.log("inspirar");
+function example() {
+  let r = int(random(4));
+  console.log('RAMDON:', r);
+  img_example = loadImage('exemplo_usuario/exemplo' + str(r) + '.png');
+  //name_image = 'exemplo_usuario/exemplo2.png'
+  console.log("inspirar");
 }
